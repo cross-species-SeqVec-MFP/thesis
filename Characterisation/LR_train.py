d70855afbddbbe2cfcb9d09e678e1423d419e54c
cross-species-SeqVec-MFP <69600847+cross-species-SeqVec-MFP@users.noreply.github.com>
2021-02-22 20:50:41 +0100

    Update and rename Improve_characterize/LR_train.py to Characterisation/LR_train.py

--- Characterisation/LR_train.py
+++ Characterisation/LR_train.py
@@ -6,10 +6,10 @@ import sys
 
 Cvalue = [[1e-3], [1e-1], [10], [1e-2], [1], [100]]
 Cs = Cvalue[int(sys.argv[1])]
-num_fea = int(sys.argv[2]) #3072, 5120 or 10240
+num_fea = int(sys.argv[2])
 
 #this data is scaled already
-with open('/tudelft.net/staff-bulk/ewi/insy/DBL/ivandenbent/final/improve_seqvec/data/XYdata_genmean_scaled_train', 'rb') as fw:
+with open('/somedirectory/XYdata_genmean_scaled_train', 'rb') as fw:
     X_train, Y_train, GO_terms = pickle.load(fw)
 print('from baseline')
 
@@ -23,5 +23,5 @@ sys.stdout.flush()
 clf = MultiOutputClassifier(SGDClassifier(loss='log', penalty='l2', alpha=C, max_iter=1000, random_state=0))
 clf.fit(X_train, Y_train)
 
-with open('/tudelft.net/staff-bulk/ewi/insy/DBL/ivandenbent/final/improve_seqvec/generalized_means/Trained_LR/c%s_LR_numfea%s_1024LSTM1.pkl' % (C, num_fea), 'wb') as fw:
+with open('/somedirectory/Trained_LR/c%s_LR_numfea%s_1024LSTM1.pkl' % (C, num_fea), 'wb') as fw:
     pickle.dump({'trained_model': clf}, fw, protocol=4)
