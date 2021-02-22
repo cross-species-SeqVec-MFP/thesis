d5c5977e45ad4e430eba3ed49ac2643b1e657c06
cross-species-SeqVec-MFP <69600847+cross-species-SeqVec-MFP@users.noreply.github.com>
2021-02-22 20:29:37 +0100

    Update and rename Improve_characterize/pred_protein_length.py to Characterisation/pred_protein_length.py

--- Characterisation/pred_protein_length.py
+++ Characterisation/pred_protein_length.py
@@ -14,13 +14,13 @@ from sklearn.utils import resample
 
 Cvalue = [100, 10, 1, 0.1, 0.01, 0.001, 0.0001, 0.00001]  # for regularization in log reg
 
-with open('/tudelft.net/staff-bulk/ewi/insy/DBL/ivandenbent/Proof_moments/optimize/Scaled_data/XYdata_moment_train', 'rb') as fw:
+with open('/somedirectory/XYdata_moment_train', 'rb') as fw:
     Xtrain, Ytrain = pickle.load(fw)
 
-with open('/tudelft.net/staff-bulk/ewi/insy/DBL/ivandenbent/Proof_moments/optimize/Scaled_data/XYdata_moment_valid', 'rb') as fx:
+with open('/somedirectory/XYdata_moment_valid', 'rb') as fx:
     Xvalid, Yvalid = pickle.load(fx)
 
-with open('/tudelft.net/staff-bulk/ewi/insy/DBL/ivandenbent/Proof_moments/optimize/Scaled_data/XYdata_moment_test', 'rb') as fz:
+with open('/somedirectory/XYdata_moment_test', 'rb') as fz:
     Xtest, Ytest = pickle.load(fz)
 
 
@@ -31,12 +31,11 @@ Xtest = Xtest[:, np.r_[0:1024]]
 enc = OneHotEncoder(handle_unknown='ignore')
  
 def len_aa(file_names_id):
-    id = np.loadtxt('/tudelft.net/staff-bulk/ewi/insy/DBL/ameliavm/'
-                    'sequence-only/lists/' + file_names_id, dtype=str).tolist()
+    id = np.loadtxt('/somedirectory/lists/' + file_names_id, dtype=str).tolist()
     length = np.zeros((len(id), ))
     type = '.pkl'
     for i, x in enumerate(id):
-        with open('/tudelft.net/staff-bulk/ewi/insy/DBL/ameliavm/sequence-only/dataset/' + x + type, 'rb') as f:
+        with open('/somedirectory/dataset/' + x + type, 'rb') as f:
             protein_info = pickle.load(f)
         length[i] = len(protein_info['sequence'])
 
@@ -59,8 +58,7 @@ for Cs in Cvalue:
 
     Ypost = clf.predict_proba(Xvalid)
 
-    with open('/tudelft.net/staff-bulk/ewi/insy/DBL/ivandenbent/Proof_moments/optimize/predictions/'
-              'moments/validation/c%spredict_length.pkl' % Cs, 'wb') as fw:
+    with open('/somedirectory/c%spredict_length.pkl' % Cs, 'wb') as fw:
         pickle.dump({'Yvalid': Y_valid, 'Ypost': Ypost, 'trained_model': clf}, fw)
 
     sys.stdout.flush()
@@ -70,8 +68,7 @@ for Cs in Cvalue:
 # validation results
 res = np.zeros(len(Cvalue))
 for joe, C in enumerate(Cvalue):
-    with open('/tudelft.net/staff-bulk/ewi/insy/DBL/ivandenbent/Proof_moments/optimize/predictions/'
-              'moments/validation/c%spredict_length.pkl' % C, 'rb') as fw:
+    with open('/somedirectory/c%spredict_length.pkl' % C, 'rb') as fw:
         data = pickle.load(fw)
 
     Ypost = np.zeros((data['Yvalid'].shape))
@@ -83,21 +80,18 @@ for joe, C in enumerate(Cvalue):
 bestC = Cvalue[np.argmax(res)]
 print('best C %s' % bestC)
 
-with open('/tudelft.net/staff-bulk/ewi/insy/DBL/ivandenbent/Proof_moments/optimize/predictions/'
-          'moments/validation/c%spredict_length.pkl' % bestC, 'rb') as fw:
+with open('/somedirectory/c%spredict_length.pkl' % bestC, 'rb') as fw:
     data = pickle.load(fw)
 
 ccc = data['trained_model']
 Ypost = ccc.predict_proba(Xtest)
 
-with open('/tudelft.net/staff-bulk/ewi/insy/DBL/ivandenbent/Proof_moments/optimize/predictions/'
-          'moments/validation/c%spredict_lengthtest.pkl' % bestC, 'wb') as fw:
+with open('/somedirectory/c%spredict_lengthtest.pkl' % bestC, 'wb') as fw:
     pickle.dump({'Yvalid': Y_test, 'Ypost': Ypost}, fw)
 
 ####
 bestC=0.0001
-with open('/tudelft.net/staff-bulk/ewi/insy/DBL/ivandenbent/Proof_moments/optimize/predictions/'
-          'moments/validation/c%spredict_lengthtest.pkl' % bestC, 'rb') as fw:
+with open('/somedirectory/c%spredict_lengthtest.pkl' % bestC, 'rb') as fw:
     data = pickle.load(fw)
 
 Ypost1 = np.zeros((data['Yvalid'].shape))
@@ -106,7 +100,7 @@ for jj, pred1 in enumerate(data['Ypost']):
 
 print(roc_auc_score(data['Yvalid'], Ypost1, average=None))
 
-with open('/tudelft.net/staff-bulk/ewi/insy/DBL/ivandenbent/Proof_moments/optimize/Seeds_random_state', 'rb') as f:
+with open('/somedirectory/Seeds_random_state', 'rb') as f:
     seeds = pickle.load(f)
 boot_results_rocauc = np.zeros((len(seeds), 10))
 
